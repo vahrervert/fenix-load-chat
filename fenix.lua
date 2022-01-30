ce4b3b0f3393dcf555d169a66f86f183c0d9e529
limonmavis <53304059+vahrervert@users.noreply.github.com>
2022-01-30 17:51:52 +0300

    Update fenix.lua

--- fenix.lua
+++ fenix.lua
@@ -1,6 +1,7 @@
- local args = {
+     local args = {
                    [1] = "|- Fenix Loaded for User " .. game.Players.LocalPlayer.Name .. " -|";
                    [2] = "All"
                    }
-
+            if _G.loadchat == true then
                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+            end
