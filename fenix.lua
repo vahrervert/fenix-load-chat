1b28ff5a79ae3f33089d0952c3a867f8ce518636
limonmavis <53304059+vahrervert@users.noreply.github.com>
2022-01-30 17:30:44 +0300

    Create fenix.lua

---
+++ fenix.lua
@@ -0,0 +1,6 @@
+ local args = {
+                   [1] = "-| Fenix Loaded for User " .. game.Players.LocalPlayer.Name;
+                   [2] = "All"
+                   }
+
+                   game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
